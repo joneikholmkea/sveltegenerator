d2782922e34dc349a06a0b06caac846bac3810aa
Jon Eikholm <jone@kea.dk>
2020-11-16 15:54:06 +0100

    steps 0 - 4

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,14 @@
 <script>
 import Generator from './Generator.svelte'
-import Users from './Users.svelte'
-import Persons from './Persons.svelte'
+// import Users from './Users.svelte'
+
 
 
 </script>
 
-<!-- <h2>Persons</h2> -->
-<h2>Svelte file Generator ver. 0.2</h2>
-<h3>Using Firebase Cloud Firestore</h3>
-<!-- <Users></Users> -->
+<h2>Svelte file generator ver. 0.3</h2>
+<h4>Will generate full CRUD functionality, using Firebase Cloud Firestore</h4>
 
-<!-- <Persons/> -->
 <Generator/>
+<p>Developed by <a href="mailto: joneikholm@gmail.com">Jon Eikholm</a> 2020</p>
+<p>Inspired by <a href="https://www.youtube.com/watch?v=Rr2kKjYIYRM">Coders Page</a></p>
--- src/Generator.svelte
+++ src/Generator.svelte
@@ -4,7 +4,7 @@ import Field from './Field.svelte'
 import { saveAs } from 'file-saver';
 let fields = ["name", "email"]
 let field = 'Name'
-let className = 'User'
+let className = 'Person'
 let button1Text = 'Download file: '+className+'.svelte'
 let button2Text = 'Download file: '+className+'s.svelte'
 
@@ -196,30 +196,84 @@ function getGridTemplateColumns(){
 </script>
 
 <!-- ###################### -->
-
-<div id="nameofclass">
-    <form on:submit|preventDefault={addNameOfClass}>
-        <input type="text" bind:value={className}>
-        <button>Save Class name</button>
-    </form>
-</div>
-<div id="addField">
-    <form on:submit|preventDefault={addField}>
-        <input type="text" bind:value={field}>
-        <button>Add field</button>
-    </form>
-</div>
-
-<div id="users">
-{#each fields as f}
-    <Field fieldName = {f} on:delete_field={deleteField} />
-{/each}
+<div id="step0" class="steps">
+    <h2>Step 0</h2>
+    <p>If you don't have a Svelte project already, do the following:</p>
+    <ul>
+    <li>download and install <a href="https://nodejs.org/en/download/">Node.js</a></li>
+    <li> download and unzip <a href="https://www.dropbox.com/s/wbco7n4j835x49x/svelte-app.zip?dl=1">this starter project</a> to <i>myFolder</i></li>
+    <li> open a terminal at <i>myFolder</i> and type:
+        <ul class="sourcecode">
+            <li>npm install + ENTER</li>
+            <li>npm run dev + ENTER</li>
+        </ul>
+    </li>
+    <li>now open a browser, and go to localhost:5000, and see if it works</li>
+    <li>create a <a href="https://firebase.google.com/docs/web/setup">Firebase project</a> </li>
+    <li>add this <a href="https://www.dropbox.com/s/2afem5lbt5w020y/firestore.js?dl=1">firestore.js</a> file to your src folder, and add your Firebase values to it.</li>
+    
+    </ul>
 </div>
 <br/>
-<button on:click={generateEntityFile}>
-{button1Text}
-</button><br/>
-<button on:click={generateCollectionFile}>
-{button2Text}
-</button>
+<div class="steps">
+    <h2>Step 1</h2>
+    <p>Enter a class name (e.g. User, Student, Animal): </p>
+    <div id="nameofclass">
+        <form on:submit|preventDefault={addNameOfClass}>
+            <input type="text" bind:value={className}>
+            <button>Save Class name</button>
+        </form>
+    </div>
+    <p>Add fields, (e.g. name, e-mail, address): </p>
+    <div id="addField">
+        <form on:submit|preventDefault={addField}>
+            <input type="text" bind:value={field}>
+            <button>Add field</button>
+        </form>
+    </div>
+
+    <div id="users">
+    {#each fields as f}
+        <Field fieldName = {f} on:delete_field={deleteField} />
+    {/each}
+    </div>
+</div>
+<br/>
+<div class="steps">
+    <h2>Step 2</h2>
+    <p>Download these 2 files, and move them into the <i>src</i> folder of your project</p>
+    <button on:click={generateEntityFile}>
+    {button1Text}
+    </button><br/>
+    <button on:click={generateCollectionFile}>
+    {button2Text}
+    </button>
+</div>
+<!-- https://www.dropbox.com/s/wbco7n4j835x49x/svelte-app.zip?dl=0 -->
+<br/>
+<div id="step2" class="steps">
+    <h2>Step 3</h2>
+    <p>Add the following line between &lt;script&gt &lt/script&gt tags in your App.svelte file:</p>
+    <p class="sourcecode">import {className} from './{className}.svelte'</p>
+</div>
+<br/>
+<div id="step3" class="steps">
+    <h2>Step 4</h2>
+    <p>Add the following line to your App.svelte file (outside the &lt;script&gt &lt/script&gt tags) :</p>
+    <p class="sourcecode">&lt;{className}s/&gt</p>
+</div>
 <!-- ###################### -->
+<style>
+.sourcecode {
+    font-family: Consolas,monaco,monospace;
+}
+
+.steps {
+    padding-left: 10px;
+    border-style: solid;
+    border-width: 1px;
+     border-color:black;
+}
+
+
+</style>
