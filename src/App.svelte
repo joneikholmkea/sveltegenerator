3461a822762eb24f94fe2c2c636ab0412c23e14c
Jon Eikholm <jone@kea.dk>
2020-11-17 00:33:34 +0100

    added link to video2

--- src/App.svelte
+++ src/App.svelte
@@ -7,8 +7,8 @@ import Generator from './Generator.svelte'
 </script>
 
 <h2>Svelte file generator ver. 0.3</h2>
-<h4>Will generate full CRUD functionality, using Firebase Cloud Firestore. <a href="https://screencast-o-matic.com/watch/cYXD0oLfJI">See 7-min video</a></h4>
-
+<h3>Will generate full CRUD functionality, using Firebase Cloud Firestore. <a href="https://screencast-o-matic.com/watch/cYXD0oLfJI">See 7-min video</a></h3>
+<p>This is meant to be a starting point for your web application. And when you need to add to existing projects.</p>
 <Generator/>
 <p>Developed by <a href="mailto: joneikholm@gmail.com">Jon Eikholm</a> 2020</p>
 <p>Inspired by <a href="https://www.youtube.com/watch?v=Rr2kKjYIYRM">Coders Page</a></p>
